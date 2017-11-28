56bc7877a673984e80d6d161600241c8a0b2d25c
Asfar MR <asfarmr@gmail.com>
2017-11-28 23:49:59 +0100

    modif fichier trigger sql

--- bd/SQL/fonctionnalite.sql
+++ bd/SQL/fonctionnalite.sql
@@ -2,7 +2,7 @@
 /*----------------------Gerer les produit----------------------*/
 /*-------------------------------------------------------------*/
 /*Ajouter un produit*/
-Insert into PRODUIT (ID_PRODUIT,LIBELLEPRODUIT) values (1,'Bonbons');
+Insert into PRODUIT (LIBELLEPRODUIT) values ('Lait');
 
 
 
@@ -10,14 +10,14 @@ Insert into PRODUIT (ID_PRODUIT,LIBELLEPRODUIT) values (1,'Bonbons');
 /*Modifier un produit (ici le numero 1)*/
 UPDATE PRODUIT
 SET libeleProduit = 'nouvelle valeur'
-WHERE id_produit = 1
+WHERE id_produit = 2;
 
 
 
 
 /*Supprimer un produit (ici le numero 1)*/
 DELETE FROM PRODUIT
-WHERE id_produit = 1
+WHERE id_produit = 2;
 
 
 
@@ -26,31 +26,33 @@ WHERE id_produit = 1
 /*-------------------------------------------------------------*/
 
 /*Creer le compte d'un enfant */
-Insert into ENFANT (ID_ENFANT,MAILCONTACTENFANT,TELCONTACTENFANT,ID_PERSONNE,ID_CATEGORIE) values (1,'pierremartin@gmail.com','0102030405',1,1);
+Insert into Personne (nompersonne,prenompersonne) values ('martin','pierre');
+Insert into ENFANT (MAILCONTACTENFANT,TELCONTACTENFANT,ID_PERSONNE,ID_CATEGORIE) values ('pierremartin@gmail.com','0102030405',21,1);
+
 
 /*Creer un compte d'un parent membre*/
-Insert into PERSONNE (ID_PERSONNE,NOMPERSONNE,PRENOMPERSONNE) values (1,'Martin','Pierre');
+Insert into PERSONNE (NOMPERSONNE,PRENOMPERSONNE) values ('Martin','Pierre');
 
 /*Supprimer un compte d'un parent membre  (ici le numero 1)*/
 DELETE FROM PERSONNE
-WHERE id_Personne = 1
+WHERE id_Personne = 1;
 
 /*Supprimer un compte d'un enfant (ici le numero 1)*/
 DELETE FROM ENFANT
-WHERE id_enfant = 1
+WHERE id_enfant = 1;
 
 /*-------------------------------------------------------------*/
 /*-------------------Gerer les produit consome-----------------*/    
 /*-------------------------------------------------------------*/
 
 /*ajouter ce que l'enfant a consome*/
-Insert into CONSOMME (DATECONSOMMATION,QTECONSOMMATION,ID_PRODUIT,ID_ENFANT,ID_PERSONNE) values (to_date('26-11ÔÂ-17','DD-MON-RR'),1,1,1,1);
+Insert into CONSOMME (DATECONSOMMATION,QTECONSOMMATION,ID_PRODUIT,ID_ENFANT,ID_PERSONNE) values (sysdate,1,2,1,11);
 
 /*supprimer ce que l'enfant a consome*/
-DELETE FROM PRODUIT
+DELETE FROM consomme
 WHERE id_Produit = 1
 AND ID_ENFANT = 1
-AND ID_PERSONNE = 1
+AND ID_PERSONNE = 11;
 
 
 
@@ -62,28 +64,12 @@ AND ID_PERSONNE = 1
 /*Consulter les stock*/
 
 SELECT STOCK
-FROM  PRODUIT_SIMPLE
+FROM  PRODUIT_SIMPLE;
 
 /*Indiquer montant total de l'achat (ici le numero 1)*/
 SELECT montantCourse
 FROM COURSE
-WHERE idCourse = 1
+WHERE idCourse = 1;
 
 
 /*?????????????????/Mettre a jour le stock de produit ????????????/*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
