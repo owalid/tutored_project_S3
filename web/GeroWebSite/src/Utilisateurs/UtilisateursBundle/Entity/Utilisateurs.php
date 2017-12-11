7a9bbcf064f66782963b884744654d2fb473ce12
Your Name <you@example.com>
2017-12-11 22:37:23 +0100

    ajout du bundle fosUser

---
+++ web/GeroWebSite/src/Utilisateurs/UtilisateursBundle/Entity/Utilisateurs.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Othmane
+ * Date: 08/12/2017
+ * Time: 15:38
+ */
+// src/AppBundle/Entity/User.php
+
+namespace Utilisateurs\UtilisateursBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="utilisateur")
+ */
+class Utilisateurs extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
